54ec05235de0b472f96d55222340e56b0563f914
nehemiah.mungau <nehemiah.mungau@libertyafrika.co.ke>
2023-08-07 11:44:59 +0200

    add Id to productdto

--- Services/DIRS21.API/Models/DTOs/Responses/ProductDto.cs
+++ Services/DIRS21.API/Models/DTOs/Responses/ProductDto.cs
@@ -4,6 +4,7 @@ namespace DIRS21.API.Models.DTOs.Responses
 {
     public class ProductDto
     {
+        public string Id { get; set; }
         public ProductCategory CategoryName { get; set; }
 
         public int Capacity { get; set; }
